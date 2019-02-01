264ff99424b2be959a87f3c6794c295d82036024
Squidysquid1 <46660298+Squidysquid1@users.noreply.github.com>
2019-02-01 11:04:52 -0600

    initial comit

---
+++ SpotPlay_toSoundPlay.py
@@ -0,0 +1,15 @@
+import time
+import subprocess
+#TODO: try a bunch of diffrent browesers and promt the user the preffered browser.
+#TODO: add a gui with tkinter
+  
+def openTab(searchTerms):
+  #parse search terms just enter url for now
+  p = subprocess.Popen(["chrome", searchTerms])
+  #TODO: check when the subprocess is killed then open new tab.
+  poll = p.poll()
+
+def checkPStatus():
+  if poll != None:
+    #open new tab thanks
+    openTab("soundcloud.com/search?q=stairway%20to%20heaven")
