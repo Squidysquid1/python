5db3945419036c44e04155479f454742658e115b
Squidysquid1 <46660298+Squidysquid1@users.noreply.github.com>
2019-02-01 11:20:01 -0600

    Added comment and parse term function

--- SpotPlay_toSoundPlay.py
+++ SpotPlay_toSoundPlay.py
@@ -2,14 +2,16 @@ import time
 import subprocess
 #TODO: try a bunch of diffrent browesers and promt the user the preffered browser.
 #TODO: add a gui with tkinter
-  
-def openTab(searchTerms):
+
+def parseTerms(termsA):
+  baseUrl="soundcloud.com/search?q=" #spaces should be replaced by"%20"
+def openTab(url):
   #parse search terms just enter url for now
-  p = subprocess.Popen(["chrome", searchTerms])
+  p = subprocess.Popen(["chrome", url])
   #TODO: check when the subprocess is killed then open new tab.
   poll = p.poll()
 
 def checkPStatus(): #if returns None thennnnn its running <3
   if poll != None:
-    #open new tab thanks
-    openTab("soundcloud.com/search?q=stairway%20to%20heaven")
+    #open new tab thanks next in array pls
+    openTab("www.soundcloud.com/search?q=stairway%20to%20heaven")
