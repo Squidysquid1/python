299fa09dac325cca79ed7d34fb5d765d95a8bc08
Squidysquid1 <46660298+Squidysquid1@users.noreply.github.com>
2019-02-01 13:30:59 -0600

    cleaned up checkPStatus
    added parameter

--- SpotPlay_toSoundPlay.py
+++ SpotPlay_toSoundPlay.py
@@ -14,11 +14,11 @@ def openTab(url):
   p = subprocess.Popen(["chrome", url])
   #TODO: check when the subprocess is killed then open new tab.
   
-def checkPStatus(): #if returns None thennnnn its running <3
+def checkPStatus(url): #if returns None thennnnn its running <3
   poll = p.poll()
   if poll != None:
     #open new tab thanks next in array pls
-    openTab("www.soundcloud.com/search?q=stairway%20to%20heaven")
+    openTab(url)
 def getJSON(url):
   jsonurl = urlopen(url)
   txt = json.loads(jsonurl.read())
