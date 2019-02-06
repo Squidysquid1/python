3d5efa79dd7725433a2fffd3ee0ffeabb7110039
Squidysquid1 <46660298+Squidysquid1@users.noreply.github.com>
2019-02-05 19:11:42 -0600

    added buggy gui

--- SpotPlay_to_SoundPlay.py
+++ SpotPlay_to_SoundPlay.py
@@ -4,12 +4,14 @@ import os
 import spotipy
 import spotipy.oauth2 as oauth2
 import sys
+import tkinter
+from tkinter import *
 #TODO: try a bunch of diffrent browesers and promt the user the preffered browser. and os
-#TODO: add a gui with tkinter
+
 
 def parseTerms(termsA):
-  baseUrl="soundcloud.com/search?q="
-  parsedURL = baseUrl + re.sub(' ','%20',termsA)#replacing spaces with 20%|| add in "illegal" chars
+  baseUrl="soundcloud.com/search?q=" #spaces should be replaced by"%20"
+  parsedURL = baseUrl + re.sub(' ','%20',termsA)#replacing spaces with 20%
   return parsedURL
 
 def openTab(url):
@@ -25,8 +27,8 @@ def checkPStatus(process): #if returns None thennnnn its running <3
 def generate_token():
     """ Generate the token. Please respect these credentials :) """
     credentials = oauth2.SpotifyClientCredentials(
-        client_id="CLIENTID",#make these system variables
-        client_secret="CLIENTSECRET")
+        client_id="client_id",#make these system variables
+        client_secret="client_secret")
     token = credentials.get_access_token()
     return token
 
@@ -43,7 +45,7 @@ def getSpotifySongs(username, playlist_id):
                 else:
                     track = item
                 try:
-                    track_name = track['name']+" by: "+track['artists'][0]['name']
+                    track_name = track['name']+" by "+track['artists'][0]['name']
                     rawData.append(track_name)
                 except KeyError:
                     print("Skipping track {0} by {1} (local only?)".format(
@@ -60,12 +62,67 @@ CHROME = os.path.join('C:\\', 'Program Files (x86)', 'Google', 'Chrome', 'Applic
 token = generate_token()
 spotify = spotipy.Spotify(auth=token)
 
-toFormatDataA = getSpotifySongs(sys.argv[1], sys.argv[2])
-formattedURL = []
-for data in toFormatDataA:
-  formattedURL.append(parseTerms(data))
-  
-#run this once everthing has ran its course
-for x in range(0,len(formattedURL)):
-    checkPStatus(openTab(formattedURL[x]))
 
+
+root = Tk()
+root.title('Spot to sound')
+root.geometry('600x400')
+
+Label(text='Enter Spotify User ID').pack(side=TOP,padx=10,pady=3)
+userID = Entry(root, width=10)
+userID.pack(side=TOP,padx=10,pady=10)
+
+Label(text='Enter Spotify Playlist ID').pack(side=TOP,padx=10,pady=3)
+playID = Entry(root, width=10)
+playID.pack(side=TOP,padx=10,pady=10)
+
+def onSubmit():
+    
+    print(userID.get(),playID.get())
+    token = generate_token()
+    spotify = spotipy.Spotify(auth=token)
+
+    toFormatDataA = getSpotifySongs(userID.get(), playID.get())
+    formattedURL = []
+    for data in toFormatDataA:
+      formattedURL.append(parseTerms(data))
+
+    #run this once everthing has ran its course
+    for x in range(0,len(formattedURL)):
+        checkPStatus(openTab(formattedURL[x]))
+
+
+def close():
+    exit()
+Button(root, text='Submit', command=onSubmit).pack(side=TOP,padx=10,pady=3)
+Button(root, text='CLOSE', command=close).pack(side=TOP,padx=10,pady=3)
+
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#21u7mi2i7v3rzw5yrsysjvu4q 485v3KtwzJQjV6TH5dUSDq
